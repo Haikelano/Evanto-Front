e808dcd0f1c2e148a49e9406d4ca317864a6c727
Haikelano <haykel.maaoui@gmail.com>
2019-04-01 02:25:55 +0200

    add config bootsrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AceuilComponent } from './aceuil/aceuil.component';
+import { IndexComponent } from './aceuil/index/index.component';
+import { NavComponent } from './aceuil/nav/nav.component';
+import { SlidebarComponent } from './aceuil/slidebar/slidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AceuilComponent,
+    IndexComponent,
+    NavComponent,
+    SlidebarComponent
   ],
   imports: [
     BrowserModule,
