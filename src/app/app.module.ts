a9353739a7bc8d3d50e02064eb6edb16916dd8a8
Haikelano <haykel.maaoui@gmail.com>
2019-04-09 01:55:03 +0200

    add component for App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AceuilComponent } from './aceuil/aceuil.component';
+import { IndexComponent } from './aceuil/index/index.component';
+import { NavComponent } from './aceuil/nav/nav.component';
+import { SlideComponent } from './aceuil/slide/slide.component';
+import { FooterComponent } from './aceuil/footer/footer.component';
+import { UsersComponent } from './users/users.component';
+import { EvenementComponent } from './evenement/evenement.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    AceuilComponent,
+    IndexComponent,
+    NavComponent,
+    SlideComponent,
+    FooterComponent,
+    UsersComponent,
+    EvenementComponent,
     ],
   imports: [
     BrowserModule,
