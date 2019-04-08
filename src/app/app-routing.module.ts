6333f81ca071f820d324b1c06922db41d18e664d
Haikelano <haykel.maaoui@gmail.com>
2019-04-09 01:28:14 +0200

    fix first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppModule } from './app.module';
-import { AceuilComponent } from './aceuil/aceuil.component';
-import { IndexComponent } from './aceuil/index/index.component';
-import { NavComponent } from './aceuil/nav/nav.component';
+
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {path: "**" ,component:AppComponent},
-  {path: "aceuil", component: AceuilComponent},
-  {path: "index", component: IndexComponent},
-  {path: "nav", component: NavComponent}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AceuilComponent } from './aceuil/aceuil.component';
-import { IndexComponent } from './aceuil/index/index.component';
-import { NavComponent } from './aceuil/nav/nav.component';
-import { SlidebarComponent } from './aceuil/slidebar/slidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AceuilComponent,
-    IndexComponent,
-    NavComponent,
-    SlidebarComponent
-  ],
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
